56f23c3b2860025e09fc6b3f68e3736b11420107
--help <marcus.zou@gmail.com>
2024-12-17 19:49:52 -0700

    Minor touch-up

--- taskingApp/src/main.py
+++ taskingApp/src/main.py
@@ -9,7 +9,7 @@ def main(page: ft.Page):
 
     def addTask(p):
         checkBox = ft.Checkbox(value=False)
-        checkBoxText = ft.Text(value=textField.value, width=350, bgcolor="WHITE", size=16)
+        checkBoxText = ft.Text(value=textField.value, width=350, bgcolor="WHITE", color="BLACK", size=16)
         taskRow = ft.Row(controls=[checkBox, checkBoxText], alignment=ft.MainAxisAlignment.START)
         page.add(taskRow)
 
@@ -20,7 +20,7 @@ def main(page: ft.Page):
     page.bgcolor = "WHITE"  ## Go to colothunt.co to pick up the colors
 
     ## Add widgets
-    textField = ft.TextField(width=400)
+    textField = ft.TextField(width=400, color="BLUE")
     addBtn = ft.FloatingActionButton(icon=ft.icons.ADD, on_click=addTask)
     entriesRow = ft.Row(controls=[textField, addBtn], 
                         alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
